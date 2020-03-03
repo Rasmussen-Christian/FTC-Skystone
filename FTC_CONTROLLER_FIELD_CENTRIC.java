e34f45f1a6b0cb9dfd0abc2bc4aa8e1f75f303be
Christian <Christian.kento@gmail.com>
2020-03-03 16:07:28 -0600

    Added inputs from controller to control field centric drive
    Made so controller one's joystick inputs will control the speed and direction of the robot

--- FTC_CONTROLLER_FIELD_CENTRIC.java
+++ FTC_CONTROLLER_FIELD_CENTRIC.java
@@ -1,35 +1,7 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import java.util.regex.Matcher;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -38,20 +10,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import java.util.*;
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-@Autonomous(name="FTC: CONTROLLER FIELD CENTRIC", group="Linear Opmode")
-
+@TeleOp(name="FTC: CONTROLLER FIELD CENTRIC2", group="Linear Opmode")
 public class FTCControllerFieldCentric extends LinearOpMode {
 
     // Declare OpMode members.
@@ -74,7 +33,6 @@ public class FTCControllerFieldCentric extends LinearOpMode {
         M1  = hardwareMap.get(DcMotor.class, "M1");
         M2  = hardwareMap.get(DcMotor.class, "M2");
         M3  = hardwareMap.get(DcMotor.class, "M3");
-       
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
@@ -87,12 +45,26 @@ public class FTCControllerFieldCentric extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            // gets the inputs from the controller
+            float gp1l_x = -gamepad1.left_stick_x;
+            float gp1l_y = gamepad1.left_stick_y;
+            float gp1r_x = gamepad1.right_stick_x;
+            
+            // calculates the power and angel
+            double power = Math.sqrt((gp1l_x*gp1l_x) + (gp1l_y*gp1l_y));
+            double angle = Math.atan2(gp1l_x,gp1l_y) + 3.14;
+            
             // makes the robot move at a designated angel
-            double rad = -3.14/4;
-            M0.setPower(1*Math.sin(rad + (3.14/4) + 0));
-            M1.setPower(1*Math.cos(rad + (3.14/4) - 0));
-            M2.setPower(1*Math.cos(rad + (3.14/4) + 0));
-            M3.setPower(1*Math.sin(rad + (3.14/4) - 0));
+            M0.setPower(power*Math.sin(angle + (3.14/4)) + gp1r_x);
+            M1.setPower(power*Math.cos(angle + (3.14/4)) - gp1r_x);
+            M2.setPower(power*Math.cos(angle + (3.14/4)) + gp1r_x);
+            M3.setPower(power*Math.sin(angle + (3.14/4)) - gp1r_x);
+            
+            // for debug
+            telemetry.addData("power", power);
+            telemetry.addData("angle", angle);
+            telemetry.addData("rotating", gp1r_x);
+            telemetry.update();
         }
     }
 }
